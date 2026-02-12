da1d1e7c754282496aee4e09c7755d0aa0be8c5a
Desmond Tatilian <info@launchthatbrand.com>
2026-02-12 15:26:09 -0500

    Add Google Analytics tracking to layout. Integrated Google Tag Manager script and initialization code for enhanced tracking capabilities on the site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { type Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -35,6 +36,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T1E6S7RMQ1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T1E6S7RMQ1');
+          `}
+        </Script>
         {children}
 
       </body>
